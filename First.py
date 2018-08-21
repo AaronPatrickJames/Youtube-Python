d25d51f26066afe129da4bc3b1f6010ba578943d
Aaron James <36738943+AaronPatrickJames@users.noreply.github.com>
2018-08-21 12:46:36 -0400

    First Video
    This is the print and hello world check, as well as comments and var sets

---
+++ First.py
@@ -0,0 +1,17 @@
+# Three Things
+
+# Comments
+# Print to Screen
+# Var Dec
+
+#printing to the screen
+print("Hello World")
+
+#printing with var
+#define var
+statement = "World" #String, Int, Float
+
+print("Hello" + statement)
+
+#Comments we have been doing this whole time!!!
+
